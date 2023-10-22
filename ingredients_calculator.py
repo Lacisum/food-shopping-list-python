2d95b147019b7aca7b08365dc80541246b87808d
Lacisum <150604772+Lacisum@users.noreply.github.com>
2023-10-22 22:22:19 +0200

    création d'un module séparé 'ingredients_computation.py'

--- ingredients_calculator.py
+++ ingredients_calculator.py
@@ -3,7 +3,7 @@
 from file_parsing import *
 from input_parsing import *
 from display import *
-
+from ingredients_computation import *
 
 
 
@@ -15,47 +15,6 @@ def meals_input():
     print('     "tofu basquaise, patates sautées"\n')
     res = input()
     return res
-
-
-
-
-# QUELLES SONT LES RECETTES SAISIES ET LEURS INGREDIENTS RESPECTIFS ?
-
-def get_required_meals_and_ingredients(l_entered_meals, l_meals_and_ingredients):
-    """Renvoie une liste de dictionnaires associant un plat saisi à ses ingrédients."""
-    l_required_meals_and_ingredients = []
-    for meal_and_ingredients in l_meals_and_ingredients:
-        if meal_and_ingredients['meal'] in l_entered_meals:
-            l_required_meals_and_ingredients.append(meal_and_ingredients)
-    return l_required_meals_and_ingredients
-
-
-
-
-# QUELS INGREDIENTS SONT REQUIS ET EN QUELLE QUANTITE ?
-
-def get_required_ingredients(l_entered_meals, l_meals_and_ingredients):
-    """Renvoie un dictionnaire associant chaque ingrédient à sa quantité requise
-    (les ingrédients dont la quantité requise est nulle sont ignorés)."""
-    l_required_meals_and_ingredients = get_required_meals_and_ingredients(l_entered_meals, l_meals_and_ingredients)
-    dict_required_ingredients = dict()
-    for meal_and_ingredients in l_required_meals_and_ingredients:
-        for ingredient in meal_and_ingredients['ingredients']:
-            if not ingredient in dict_required_ingredients:
-                dict_required_ingredients[ingredient] = 0
-            dict_required_ingredients[ingredient] += meal_and_ingredients['ingredients'][ingredient]
-    return dict_required_ingredients
-
-
-
-
-# QUELLE SONT LES UNITES DES QUANTITES DES INGREDIENTS REQUIS ?
-
-def get_correct_unit(ingredient, dict_units):
-    for unit in dict_units:
-        if ingredient in dict_units[unit]:
-            correct_unit = unit
-    return correct_unit
 
 
 
